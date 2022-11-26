42d3f70f2c902f809543b3ab4919ceb394254a81
lipeng0 <lipeng0@aixuexi.com>
2022-11-26 23:15:05 +0800

    feat: 增加antd button

--- src/pages/antd/message/index.jsx
+++ src/pages/antd/message/index.jsx
@@ -2,14 +2,31 @@ import React, { useState } from 'react';
 import { Button, message } from 'antd';
 import './index.less';
 
+let timer;
+
 const InteractItem = (props) => {
+  const [loading, setLoading] = useState(false);
 
   const handleClick = () => {
-    message.error('1111')
-  }
+    setLoading(true);
+    message.error('1111', 2);
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      setLoading(false);
+    }, [15000]);
+  };
+
+  const removeLoading = () => {
+    clearTimeout(timer);
+    setLoading(false);
+  };
   return (
     <div>
-      <Button onClick={handleClick} >show Message</Button>
+      <Button onClick={handleClick} loading={loading}>
+        show Message
+      </Button>
+      <br />
+      <Button onClick={removeLoading}>清除loading</Button>
     </div>
   );
 };
