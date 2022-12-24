b041406ef36ba92fb39ae7f9b4c4e7d7585774ef
lipeng0 <lipeng0@aixuexi.com>
2022-12-24 18:07:32 +0800

    refactor: 优化input样式

--- src/pages/layout/input-layout/index.jsx
+++ src/pages/layout/input-layout/index.jsx
@@ -10,28 +10,31 @@ const InteractTemplate = () => {
       <div>
         <span>
           说明：这个方案是利用
-          <span>按钮flex：1;占据剩余空间，</span>
-          <span>然后日历选择期动态长度，占据剩余空间，</span>
-          <span>然后使用媒体查询，长度太长的时候左浮</span>
+          <span>1.按钮flex：1;占据剩余空间，右浮</span>
+          <span>2.然后日历选择期flex:1动态长度，占据剩余空间，</span>
+          <span>3（1）.父级设置display:flex,可以让内容的最大宽度为子元素的宽度</span>
+          <span>3（2）.设置一个最大宽度，防止右浮超出内容区,这个稍微low一些</span>
         </span>
       </div>
-      <div id="input-layout">
-        <Input style={{ width: 120 }} placeholder="互动id" />
-        <Input style={{ width: 120 }} placeholder="互动名称" />
-        <Input style={{ width: 120 }} placeholder="创建人邮箱" />
-        <RangePicker style={{ width: 214 }} showTime />
-        <Select
-          style={{
-            width: 120,
-          }}
-        />
-        <Select
-          style={{
-            width: 120,
-          }}
-        />
-        <div className="search-button">
-          <Button type="primary">搜索</Button>
+      <div id="input-layout-container">
+        <div id="input-layout">
+          <Input style={{ width: 120 }} placeholder="互动id" />
+          <Input style={{ width: 120 }} placeholder="互动名称" />
+          <Input style={{ width: 120 }} placeholder="创建人邮箱" />
+          <RangePicker style={{ width: 214 }} showTime />
+          <Select
+            style={{
+              width: 120,
+            }}
+          />
+          <Select
+            style={{
+              width: 120,
+            }}
+          />
+          <div className="search-button">
+            <Button type="primary">搜索</Button>
+          </div>
         </div>
       </div>
     </div>
