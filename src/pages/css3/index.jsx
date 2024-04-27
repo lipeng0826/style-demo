2022c9303b3538c0baf9da3ab9e65a29e3b4445d
lipeng0 <lipeng0@aixuexi.com>
2024-04-27 19:30:30 +0800

    refactor: 优化样式

--- src/pages/css3/index.jsx
+++ src/pages/css3/index.jsx
@@ -8,6 +8,7 @@ import Juzhong from './juzhong';
 import Canvas from './canvas';
 import Svg from './svg';
 import Drop from './drop';
+import Grid from './grid';
 import Flex from './flex';
 import Icon from './icon';
 import HuanDengPian from './huan-deng-pic';
@@ -17,36 +18,36 @@ import Other from './other';
 const Css3 = (props) => {
   const [tabValue, setTabValue] = useState('weiyuansu');
 
+  // 将组件和标签页信息放入数组
+  const tabComponents = [
+    { key: 'weiyuansu', component: <WeiYuanSu />, tab: '伪元素' },
+    { key: 'transition', component: <Transition />, tab: 'Transition' },
+    { key: 'Mask', component: <Mask />, tab: 'mask' },
+    { key: 'Juzhong', component: <Juzhong />, tab: '居中' },
+    { key: 'Canvas', component: <Canvas />, tab: 'Canvas' },
+    { key: 'Svg', component: <Svg />, tab: 'Svg' },
+    { key: 'Drop', component: <Drop />, tab: 'Drop' },
+    { key: 'Grid', component: <Grid />, tab: 'Grid' },
+    { key: 'Flex', component: <Flex />, tab: 'Flex' },
+    { key: 'icon', component: <Icon />, tab: 'css图标' },
+    { key: 'HuanDengPian', component: <HuanDengPian />, tab: '幻灯片' },
+    { key: 'M-P', component: <MP />, tab: 'M-P' },
+    { key: 'other', component: <Other />, tab: '未分类' },
+  ];
+
   return (
     <div>
       <div>
-        <Tabs value="tabValue" onChange={setTabValue}>
-          <Tabs.TabPane tab="伪元素" key="weiyuansu" />
-          <Tabs.TabPane tab="Transition" key="transition" />
-          <Tabs.TabPane tab="mask" key="Mask" />
-          <Tabs.TabPane tab="居中" key="Juzhong" />
-          <Tabs.TabPane tab="Canvas" key="Canvas" />
-          <Tabs.TabPane tab="Svg" key="Svg" />
-          <Tabs.TabPane tab="Drop" key="Drop" />
-          <Tabs.TabPane tab="Flex" key="Flex" />
-          <Tabs.TabPane tab="css图标" key="icon" />
-          <Tabs.TabPane tab="幻灯片" key="HuanDengPian" />
-          <Tabs.TabPane tab="未分类" key="other" />
+        <Tabs value={tabValue} onChange={setTabValue}>
+          {/* 渲染标签页 */}
+          {tabComponents.map((item) => (
+            <Tabs.TabPane key={item.key} tab={item.tab} />
+          ))}
         </Tabs>
       </div>
       <div>
-        {tabValue === 'weiyuansu' && <WeiYuanSu />}
-        {tabValue === 'transition' && <Transition />}
-        {tabValue === 'Mask' && <Mask />}
-        {tabValue === 'Juzhong' && <Juzhong />}
-        {tabValue === 'Canvas' && <Canvas />}
-        {tabValue === 'Svg' && <Svg />}
-        {tabValue === 'Drop' && <Drop />}
-        {tabValue === 'Flex' && <Flex />}
-        {tabValue === 'icon' && <Icon />}
-        {tabValue === 'HuanDengPian' && <HuanDengPian />}
-        {tabValue === 'M-P' && <MP />}
-        {tabValue === 'other' && <Other />}
+        {/* 根据当前选中的标签页渲染对应的组件 */}
+        {tabComponents.map((item) => tabValue === item.key && item.component)}
       </div>
     </div>
   );
