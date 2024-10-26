b76da96b96c5ef260ce01fa7f5820a7a22276618
lipeng <p_lipeng@ledupeiyou.com>
2024-10-26 17:43:52 +0800

    feat: add code

--- src/pages/element/box/box-style-4/index.jsx
+++ src/pages/element/box/box-style-4/index.jsx
@@ -4,34 +4,44 @@ import addSvg from './assets/add.svg';
 
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 
-const InteractItem = (props) => {
+
+const ChildItem = () => {
 
   const [choose, setChoose] = useState(false);
 
   const onClick = () => {
     setChoose(true)
   }
-  
+
   const clickOperate = (e) => {
     e.stopPropagation();
     console.log('操作');
   }
 
+  return <div className={`item-container  ${choose ? 'active' : ''}`} onClick={onClick}>
+    <div className="operate" onClick={clickOperate}>
+      <span style={{ marginRight: 4 }}><EditOutlined /></span>
+      <span><DeleteOutlined /></span>
+    </div>
+    <div className="top">
+      {/* 目前场景字符长度都是2 */}
+      <div className="tag-scenario">场景</div>
+      <div className="tag-reference">参考强度80</div>
+    </div>
+    <div className="item-name">名字名字名字名字名字名字名字名字名字名字名字名字名字</div>
+  </div>
+}
+
+const InteractItem = (props) => {
+
+  const [array] = useState([1]);
+
   return (
     <>
       <div style={{ display: 'flex' }}>
-        <div className={`item-container  ${choose ? 'active' : ''}`} onClick={onClick}>
-          <div className="operate" onClick={clickOperate}>
-            <span style={{ marginRight: 4 }}><EditOutlined /></span>
-            <span><DeleteOutlined /></span>
-          </div>
-          <div className="top">
-            {/* 目前场景字符长度都是2 */}
-            <div className="tag-scenario">场景</div>
-            <div className="tag-reference">参考强度80</div>
-          </div>
-          <div className="item-name">名字名字名字名字名字名字名字名字名字名字名字名字名字</div>
-        </div>
+        {
+          array.map(item => <ChildItem key={item} />)
+        }
         <div className='item-container empty'>
           <img src={addSvg} />
         </div>
