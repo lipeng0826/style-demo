2d14419044b07af9412ead5fb784758167e7ff29
lipeng <p_lipeng@ledupeiyou.com>
2024-10-26 17:51:32 +0800

    feat: add code

--- src/pages/element/box/index.jsx
+++ src/pages/element/box/index.jsx
@@ -1,28 +1,22 @@
 import React, { useState } from 'react';
-import { Button, Tabs } from 'antd';
+import { Tabs } from 'antd';
 import Box1 from './box-style-1/index'
 import Box2 from './box-style-2/index'
 import Box3 from './box-style-3/index'
 import Box4 from './box-style-4/index'
 import './index.less';
 
-const InteractItem = (props) => {
+const InteractItem = () => {
+  const items = [
+    { label: '盒子1', key: '1', children: <Box1 /> },
+    { label: '盒子2', key: '2', children: <Box2 /> },
+    { label: '盒子3', key: '3', children: <Box3 /> },
+    { label: '盒子4', key: '4', children: <Box4 /> },
+  ];
+
   return (
     <div>
-      <Tabs defaultActiveKey="4">
-        <Tabs.TabPane tab="盒子1" key="1">
-          <Box1 />
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="盒子2" key="2">
-          <Box2 />
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="盒子3" key="3">
-          <Box3 />
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="盒子4" key="4">
-          <Box4 />
-        </Tabs.TabPane>
-      </Tabs>
+      <Tabs defaultActiveKey="4" items={items} />
     </div>
   );
 };
